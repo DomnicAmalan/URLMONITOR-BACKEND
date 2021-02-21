eee82962de644168600d6c31b69f8e109d11c394
dom <domnic.amalan@gitstart.dev>
2021-02-21 23:13:48 +0530

    changes

--- express/server.js
+++ express/server.js
@@ -73,23 +73,22 @@ app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
-app.use("/api/users", require("./routes/userRoutes"));
 
-// mongoose.connect('mongodb+srv://domnic:0308SDAssa@cluster0.wptgp.mongodb.net/surveysp?retryWrites=true&w=majority', 
-//   {useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useFindAndModify: false,
-//     useCreateIndex: true
-//   }, 
-//   (err) => {
-//       if (!err) {
-//           console.log('Successfully Established Connection with MongoDB')
-//       }
-//       else {
-//           console.log('Failed to Establish Connection with MongoDB with Error: '+ err)
-//       }
-//   }
-// );
+mongoose.connect('mongodb+srv://domnic:0308SDAssa@cluster0.wptgp.mongodb.net/surveysp?retryWrites=true&w=majority', 
+  {useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  }, 
+  (err) => {
+      if (!err) {
+          console.log('Successfully Established Connection with MongoDB')
+      }
+      else {
+          console.log('Failed to Establish Connection with MongoDB with Error: '+ err)
+      }
+  }
+);
 
 
 module.exports = app;
