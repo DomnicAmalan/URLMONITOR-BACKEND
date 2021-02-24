aa0d9ebe59790df4ed28f8a93ed6a009a831936a
dom <domnic.amalan@gitstart.dev>
2021-02-24 21:43:14 +0530

    deploy

--- routes/funtionRoutes.js
+++ routes/funtionRoutes.js
@@ -9,6 +9,7 @@ router.delete("/delete-monitor/:id", authenticateJWT, FuntionController.deleteMo
 router.post("/activate-job/:id/", authenticateJWT, FuntionController.activateDeactivateJob);
 router.get("/get-logs/:id", authenticateJWT, FuntionController.getAllLogs);
 router.get("/get-monitor/:id", authenticateJWT, FuntionController.getMonitor)
+router.post("/update-monitor/:id", authenticateJWT, FuntionController.editMonitor)
 
 
 module.exports = router;
